4fa6c9029b2ff9edf434c673a90c1632fff17b3c
cristinagalan <cristina0203@hotmail.com>
2018-08-31 20:43:15 +0200

    Empiezo proyecto con Bootstrap

--- 1.js
+++ 1.js
@@ -1,225 +1,35 @@
-window.addEventListener('load', () => {
-
-/*  ******************************************************** DECLARO LAS VARIABLES ********************************************************  */
+    window.addEventListener("load",(e) => {
+    /*  ******************************************************** DECLARO LAS VARIABLES ********************************************************  */
     var correctas = [];
     var preguntas = [];
-    var res = [];
-    var aciertos = 0;
-    var vacias = 0;
-    var fallos = 0;
-    var cont = 0;
+      /* Meto los temas */
+          /* con bootstrap */
+    var v1=document.querySelector(".dropdown-menu");
+    
+    for(var i=0;i<temas.length;i++){
+        v1.innerHTML+='<a class="dropdown-item" href="?t_ini='+temas[i].id_tema + "&t_fin=" + temas[i].id_tema + '&n=30">'+ temas[i].tema+"</a>";
+    }
+    
+    /* Dibujo las cajas de las preguntas */
 
-    var v = document.querySelector('form ul');
-    /* Fin declaración de variables */
+    v2=document.querySelector(".card");
+    for(var i=0;i<datos.length;i++){
+    	v2[i]+="<div class='card-body'></div>"
+    }
+    
+    /* Escribo las preguntas */ 
+    var v3=document.querySelectorAll(".card-body"); // llego a contenedores de preguntas
 
-/*  ********************************************************  MOSTRAR PREGUNTAS ********************************************************   */
     var c=0;
     for (var i = 0; i < datos.length; i++) {
         correctas[i] = datos[i].respuesta;
         preguntas[i] = '.preg' + datos[i].id_pregunta;
-        v.innerHTML += '<li><div class="' + preguntas[i] + '"><p>' + (i + 1) + (". ) ") + datos[i].pregunta + '</p><ul><li><input type="radio" id="f' + 1 * [i + 1+c] + '" name="p' + [i + 1] + '" value="1" class="p' + [i + 1] + '" ><label class="c' + [i + 1] + '" for="f' + 1 * [i + 1+c] + '">' + datos[i].op1 + '</label></li><li><input type="radio" id="f' + 2 * [i + 1+c] + '" name="p' + [i + 1] + '" value="2" class="p' + [i + 1] + '"><label class="c' + [i + 1] + '" for="f' + 2 * [i + 1+c] + '">' + datos[i].op2 + '</label></li><li><input type="radio" id="f' + 3 * [i + 1+c] + '" name="p' + [i + 1] + '" value="3" class="p' + [i + 1] + '"><label class="c' + [i + 1] + '" for="f' + 3 * [i + 1+c] + '">' + datos[i].op3 + '</label></li><li><input type="radio" id="f' + 4 * [i + 1+c] + '" name="p' + [i + 1] + '" value="4" class="p' + [i + 1] + '"><label class="c' + [i + 1] + '" for="f' + 4 * [i + 1+c] + '">' + datos[i].op4 + '</label></li></ul></div></li>';
+        v3[i].innerHTML += '<div class="' + preguntas[i] + '"><p>' + (i + 1) + (". ) ") + datos[i].pregunta + '</p><ul><li><input type="radio" id="f' + 1 * [i + 1+c] + '" name="p' + [i + 1] + '" value="1" class="p' + [i + 1] + '" ><label class="c' + [i + 1] + '" for="f' + 1 * [i + 1+c] + '">' + datos[i].op1 + '</label></li><li><input type="radio" id="f' + 2 * [i + 1+c] + '" name="p' + [i + 1] + '" value="2" class="p' + [i + 1] + '"><label class="c' + [i + 1] + '" for="f' + 2 * [i + 1+c] + '">' + datos[i].op2 + '</label></li><li><input type="radio" id="f' + 3 * [i + 1+c] + '" name="p' + [i + 1] + '" value="3" class="p' + [i + 1] + '"><label class="c' + [i + 1] + '" for="f' + 3 * [i + 1+c] + '">' + datos[i].op3 + '</label></li><li><input type="radio" id="f' + 4 * [i + 1+c] + '" name="p' + [i + 1] + '" value="4" class="p' + [i + 1] + '"><label class="c' + [i + 1] + '" for="f' + 4 * [i + 1+c] + '">' + datos[i].op4 + '</label></li></ul></div>';
         correctas[i] = datos[i].respuesta;
          c+=4;
 
     }
 
-    if (datos.length > 0) {
-        document.querySelector("#boton").style.opacity = 1;
-         document.querySelector(".preguntas").style.opacity = 1;
-
-    }
-   /* Fin mostrar preguntas en  pantalla */
-
-/*   ******************************************************** INTRODUCIR TÍTULOS DE LOS TEMAS  ********************************************************  */
-    var v1 = document.querySelector("#menu ul li ul");
-    console.log("temas",temas);
-    console.log("v1", v1);
-    for (var i = 0; i < temas.length; i++) {
-        v1.innerHTML += "<li><a href='?t_ini=" + temas[i].id_tema + "&t_fin=" + temas[i].id_tema + "&n=30'>"+ temas[i].tema+"</a></li>";
-    }
-
-
-
-/*  ******************************************************** SELECCIONAR LAS RESPUESTAS HACIENDO CLIC EN EL LABEL  *********************************************************/
-    var enlaces = document.querySelectorAll("ul>li>div>ul>li");
-    console.log("enlaces", enlaces);
-    var contador = 0;
-
-    enlaces.forEach((va) => {
-        va.addEventListener("click", (event) => {
-            contador++;
-            /* Comenzar a contar el cronómetro (cuando empiece a hacer el test, es decir, cuando haga click en alguna respuesta) */
-            if (contador == 1) {
-    /* ******************************************************** CRONÓMETRO ********************************************************  */
-
-                /* ********************** DECLARO LAS VARIABLES**********************  */
-                var s0 = 1;
-                var s1 = 1;
-                var m0 = 1;
-                var m1 = 1;
-                var h0 = 1;
-                var h1 = 1;
-
-
-                /*  ********************** inicializo el reloj a 0.  **********************  */
-                var dig = document.querySelectorAll(".digitos");
-                for (var i = 0; i < dig.length; i++) {
-                    dig[i].innerHTML = "0";
-                }
-                /* ********************** SEGUNDOS  ********************** */
-                setInterval(() => {
-                    document.querySelector("#s0").innerHTML = s0;
-                    if(contador) s0++; /* si contador es true 1 */
-                    if (s0 == 10) {
-                        s0 = 0;
-                    }
-                }, 1000);
-
-                setInterval(() => {
-                    document.querySelector("#s1").innerHTML = s1;
-                    if(contador)s1++;
-                    if (s1 == 6) {
-                        s1 = 0;
-                    }
-                }, 10000);
-
-                /*  **********************  MINUTOS  **********************  */
-                setInterval(() => {
-                    document.querySelector("#m0").innerHTML = m0;
-                    if(contador)m0++;
-                    if (m0 == 10) {
-                        m0 = 0;
-                    }
-                }, 60000);
-
-                setInterval(() => {
-                    document.querySelector("#m1").innerHTML = m1;
-                   if(contador) m1++;
-                    if (m1 == 6) {
-                        m1 = 0;
-                    }
-                }, 600000);
-
-                /*  **********************  HORAS  **********************  */
-
-                setInterval(() => {
-                    document.querySelector("#h0").innerHTML = h0;
-                   if(contador) h++;
-                    if (h0 == 10) {
-                        h0 = 0;
-                    }
-                }, 3600000);
-
-                setInterval(() => {
-                    document.querySelector("#h1").innerHTML = h1;
-                   if(contador) h1++;
-                    if (h1 == 3) {
-                        h1 = 0;
-                    }
-                }, 36000000);
-
-                /* ******************************************************** FIN CRONÓMETRO ********************************************************  */
-            }
-
-            var elemeto = event.target;
-            var indice = elemeto.getAttribute("for");
-            console.log(indice);
-            document.querySelector(".cronometro").style.opacity = 1; /* LO MUESTRO CUANDO HAGA CLICK*/
-  
-        })
-
-    });
-/*  ******************************************************** FUNCIÓN ENVÍA  ********************************************************  */
-    document.querySelector("#boton").addEventListener("click", () => {
-        var cuenta = 0;
-        var longitud = preguntas.length;
-        leer();
-        corregir(correctas, res);
-        var puntuacion = (aciertos * (10 / longitud) - fallos * 0.5 * (10 / longitud)); // calcula la puntuación: acierto +1 fallo -0.5
-
-        document.querySelector(".sol").innerHTML = "Contestadas: " + (aciertos + fallos) + "<br>" + "Aciertos: " + aciertos + "<br>" + " Fallos: " + fallos + "<br>" + " Preguntas en blanco: " + vacias + "<br>" + "Puntuación: " + puntuacion
-        document.querySelector(".mostrarSoluciones").style.opacity = 1; // muestro la ventana
-        cuenta++;
-        console.log(cuenta);
-
-        if (cuenta > 1) {
-            res = [];
-        };
     });
 
-    /* Fin función envía */
-
-
-
-
-    /*  ******************************************************** FUNCIÓN LEER  ******************************************************** */
-    // puedo prescindir del array con las clases
-    function leer() {
-        var contestado = 0;
-
-        for (i = 0; i < datos.length; i++) {
-            var cajas = document.querySelectorAll(".p" + (i + 1)); // le sumo 1 porque las id empiezan en 1 y la i que es el contador en 0
-            for (var j = 0; j < cajas.length; j++) {
-                if (cajas[j].checked) {
-                    res.push(cajas[j].value);
-                    contestado = 1;
-                }
-            }
-            if (contestado == 0) {
-                res.push("blanco");
-            }
-            contestado = 0;
-        }
-        console.log(res);
-        console.log(cajas);
-    }
-
-    /* Fin función leer  */
-
-    /*  ******************************************************** FUNCIÓN CORREGIR ********************************************************  */
-    function corregir(corr, resp) {
-        var v;
-        var cont;
-        var selector;
-        var sel2;
-        var listanodos = 0;
-        contador=0;
-        for (i = 0; i < corr.length; i++) {
-            selector = ".c" + [i + 1];
-            v = parseInt(document.querySelector(selector).value = corr[i]);
-            cont = parseInt(document.querySelector(selector).value = resp[i]);
-
-            listanodos = document.querySelectorAll("li>" + selector);
-
-            /* Si acierta la pregunta */
-            if (corr[i] == resp[i]) {
-                aciertos++;
-                listanodos[v - 1].style.backgroundColor = "#ABEBC6";
-                /* Si la deja en blanco*/
-            } else if (resp[i] == "blanco") {
-                vacias++;
-                listanodos[v - 1].style.backgroundColor = "red";
-            } else { /* Si falla */
-                fallos++;
-                listanodos[v - 1].style.backgroundColor = "#ABEBC6";
-                listanodos[v - 1].style.fontWeight = "bold";
-                listanodos[cont - 1].style.backgroundColor = "red";
-            }
-
-        }
-    }
-    /* Fin función corregir */
-
-
-    /* ************************** Temporizador ************************** */
-
-    function temporizador() {
-        setTimeout(function() {
-            alert("Fin del tiempo");
-        }, 100000);
-    }
-
-    /* fin del temporizador */
-
-
-});
+ 
--- index.php
+++ index.php
@@ -1,10 +1,15 @@
-<!DOCTYPE html>
-<html lang="es">
+<!doctype html>
+<html lang="en">
+  <head>
+    <script type="text/javascript" src="1.js"></script>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
+    <title>Oposiciones</title>
     <?php 
 
         include_once "libreria.php";
@@ -37,57 +42,75 @@
     
     <script type="text/javascript" src="1.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
-    
-</head>
 
-<body>
-    <div class="wrapper">
-       <div class="izquierda"> <form>
-               <div class="temas">
-                   <h1>ENTRENADOR AUXILIAR ADMINISTRATIVO GOBIERNO DE CANTABRIA</h1>
-                <div class="filtro">                  
-                    <div class="enlinea"><label>Del tema:</label> <input type="text" name="t_ini" ></div>
-                    <div class="enlinea"><label>al tema:</label><input type="text" name="t_fin"></div>
-                    <div class="enlinea"><label>Nº de preguntas</label><input type="number" name="n" ></div>
-                   <div class="enlinea"> <input type="submit" name="ir" id="ir" value="Elegir"></div>
+  </head>
+  <body>
+    
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <div class="container mt-2 mt-sm-4 p-3 bg-primary text-white">
+              <nav class="navbar m-2 navbar-expand-lg navbar-dark bg-info" >
+            <a class="navbar-brand" href="#">Entrenador Auxiliar administrativo</a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Test por temas
+        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                           <!--  lo meto por JS -->
+                        </div>
+                    </li>
+                    
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+                </form>
+            </div>
+        </nav>
+    </div>
+    <div class="jumbotron text-primary">
+            <h1 class="display-4">ENTRENADOR AUXILIAR ADMINISTRATIVO GOBIERNO DE CANTABRIA</h1>
+</div>
+
+<div class="card-deck text-primary">
+            <div class="card">
+              
+                <div class="card-body">
                     
                 </div>
-
-               </div>
-               </form>
-               <div class="preguntas">
-                <div class="tactual"></div>
-                 <form>
-                   <ul></ul>
-                  <div class="corregir"> <input type="button" name="enviar" value="CORREGIR" id="boton"></div>
-               </form>
-               </div>
+            </div>
+            <div class="card">
                
-           </div>
-           <div class="mostrarSoluciones">
-               <div class="cerrar"><a href="index.php" title=""><i class="fa fa-window-close" aria-hidden="true"></i></a></div>
-               <div class="sol"></div>
-           </div>
-           <div class="derecha">
-              <nav id="menu">
-                         <ul>
-                           <li><a href="#"> <i class="fas fa-bars"></i>TEST POR TEMAS</a> 
-                             <ul></ul>
-                           </li>
-                          </ul>             
-              </nav>
-              <div class="cronometro">
-                        <div class="digitos" id="h1"></div>
-                         <div class="digitos" id="h0"></div>
-                         <div class="separador">:</div>
-                         <div class="digitos" id="m1"></div>
-                         <div class="digitos" id="m0"></div>
-                         <div class="separador">:</div>
-                         <div class="digitos" id="s1"></div>
-                         <div class="digitos" id="s0"></div>
-                    </div>
-          </div>
-    </div>
-</body>
+                <div class="card-body">
+                  
+                </div>
+            </div>
+            <div class="card">
+               
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
 
+
+
+    
+  </body>
 </html>
